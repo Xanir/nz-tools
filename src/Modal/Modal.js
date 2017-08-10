433f1744badb209041f28b61bcfc0226c16d1974
Joseph <Joseph.Aspelund@target.com>
2017-08-10 11:50:17 -0500

    Add start of Modal widget.

---
+++ src/Modal/Modal.js
@@ -0,0 +1,7 @@
+// Esc to close
+// Click backdrop to dismiss
+// Allow browser back to dismiss modal
+// Optional modal mask vs glass
+
+// How to build template, what model to use?
+// How to pass model back on confirm?
